e39141ace2dc118996f45456d4c50cdc8c4b48c3
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2021-12-01 09:58:50 +0300

    Day 1 completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,27 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var result = 0
+        for (index in 1 until input.size) {
+            if (input[index - 1] < input[index]) result++
+        }
+        return result
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        val windowedSums = input.windowed(size = 3).map(List<Int>::sum)
+        var result = 0
+        for (index in 1 until windowedSums.size) {
+            if (windowedSums[index - 1] < windowedSums[index]) result++
+        }
+        return result
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
